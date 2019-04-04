c5344e761adfcaedc8c4e2d47cce9f78778b8735
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-04-04 15:41:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // Write your solution in this file!
 var customerName = 'bob';
+const leastFavoriteCustomer = 'ok'
 
 function upperCaseCustomerName(){
   customerName = customerName.toUpperCase();
